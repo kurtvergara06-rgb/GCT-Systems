d33fbcc03e92da9aca44297581482eba30de770b
oclo <kurtvergara06@gmail.com>
2026-08-03 00:41:26 +0800

    Fix Render authentication redirect host

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -30,7 +30,6 @@ return Application::configure(basePath: dirname(__DIR__))
             at: '*',
             headers:
                 Request::HEADER_X_FORWARDED_FOR |
-                Request::HEADER_X_FORWARDED_HOST |
                 Request::HEADER_X_FORWARDED_PORT |
                 Request::HEADER_X_FORWARDED_PROTO
         );
