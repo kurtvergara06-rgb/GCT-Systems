6cf17869b7c5be921aed61763a848bef4d4b6220
oclo <kurtvergara06@gmail.com>
2026-08-03 09:20:59 +0800

    Load auto scheduling conflict redesign

--- resources/js/app.js
+++ resources/js/app.js
@@ -9,5 +9,8 @@ import './Login/login.js';
 import '../css/Maintenance/job-order.css';
 import './Maintenance/job-order.js';
 
+import '../css/Operation/Scheduling_And_Dispatch/auto-conflict-redesign.css';
+import './Operation/Scheduling_And_Dispatch/auto-conflict-redesign.js';
+
 import './Admin/Data_Management/batch-file-processing.js';
 import './Admin/User_Management/users.js';
