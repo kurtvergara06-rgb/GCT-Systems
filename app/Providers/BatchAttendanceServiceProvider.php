961d074db539c644017e4ff7e8b9a94af97be770
oclo <kurtvergara06@gmail.com>
2026-08-04 10:02:00 +0800

    Add personnel update and deactivate routes

--- app/Providers/BatchAttendanceServiceProvider.php
+++ app/Providers/BatchAttendanceServiceProvider.php
@@ -30,11 +30,19 @@ class BatchAttendanceServiceProvider extends ServiceProvider
                     ->name('operation.personnel.drivers');
                 Route::post('/drivers', [PersonnelController::class, 'storeDriver'])
                     ->name('operation.personnel.drivers.store');
+                Route::put('/drivers/{driver}', [PersonnelController::class, 'updateDriver'])
+                    ->name('operation.personnel.drivers.update');
+                Route::patch('/drivers/{driver}/deactivate', [PersonnelController::class, 'deactivateDriver'])
+                    ->name('operation.personnel.drivers.deactivate');
 
                 Route::get('/mechanics', [PersonnelController::class, 'mechanics'])
                     ->name('operation.personnel.mechanics');
                 Route::post('/mechanics', [PersonnelController::class, 'storeMechanic'])
                     ->name('operation.personnel.mechanics.store');
+                Route::put('/mechanics/{mechanic}', [PersonnelController::class, 'updateMechanic'])
+                    ->name('operation.personnel.mechanics.update');
+                Route::patch('/mechanics/{mechanic}/deactivate', [PersonnelController::class, 'deactivateMechanic'])
+                    ->name('operation.personnel.mechanics.deactivate');
             });
     }
 }
