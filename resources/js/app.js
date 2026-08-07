e44ba4936d3a3f1bc5828f2417555359b381fe43
oclo <kurtvergara06@gmail.com>
2026-08-07 11:58:53 +0800

    Load searchable select component globally

--- resources/js/app.js
+++ resources/js/app.js
@@ -8,12 +8,14 @@ import './echo';
  */
 import '../css/Main-styles/system-toast.css';
 import '../css/Main-styles/shared-ui-enhancements.css';
+import '../css/Main-styles/searchable-select.css';
 import '../css/Maintenance/maintenance-ui-enhancements.css';
 import './Main-js/system-toast.js';
 import './Main-js/automatic-table-search.js';
 import './Main-js/auto-id-badges.js';
 import './Main-js/shared-shell-enhancements.js';
 import './Main-js/scroll-table-pagination.js';
+import './Main-js/searchable-select.js';
 import './Maintenance/maintenance-ui-enhancements.js';
 
 /* Job Orders shared refinements loaded once through the application bundle. */
