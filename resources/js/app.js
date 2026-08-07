5951f804287b7d9c5c6778988584a4efb4f16330
oclo <kurtvergara06@gmail.com>
2026-08-07 09:27:53 +0800

    Load shared UI search and shell enhancements

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,10 @@
 import './echo';
 
 import '../css/Main-styles/system-toast.css';
+import '../css/Main-styles/shared-ui-enhancements.css';
 import './Main-js/system-toast.js';
+import './Main-js/automatic-table-search.js';
+import './Main-js/shared-shell-enhancements.js';
 
 import '../css/Login/login.css';
 import './Login/login.js';
