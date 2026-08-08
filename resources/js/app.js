b8ba5eace72a74287e29dc62395da73efd8a007f
oclo <kurtvergara06@gmail.com>
2026-08-08 19:59:42 +0800

    Enable global loading spinners

--- resources/js/app.js
+++ resources/js/app.js
@@ -10,6 +10,7 @@ import '../css/Main-styles/system-toast.css';
 import '../css/Main-styles/shared-ui-enhancements.css';
 import '../css/Main-styles/searchable-select.css';
 import '../css/Main-styles/date-time-picker.css';
+import '../css/Main-styles/spinner.css';
 import '../css/Maintenance/maintenance-ui-enhancements.css';
 import '../css/Operation/Routes/route-pin-enhancements.css';
 
@@ -20,6 +21,7 @@ import './Main-js/shared-shell-enhancements.js';
 import './Main-js/scroll-table-pagination.js';
 import './Main-js/searchable-select.js';
 import './Main-js/date-time-picker.js';
+import './Main-js/loading-state.js';
 import './Maintenance/maintenance-ui-enhancements.js';
 
 /* Shared by Driver and Mechanic Attendance pages. */
