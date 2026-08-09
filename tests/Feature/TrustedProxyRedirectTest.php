ed52a4f43e3ee5dd86a19a586032bc7b187dcc3e
oclo <kurtvergara06@gmail.com>
2026-08-09 15:35:07 +0800

    Make trusted proxy redirect test use request host header

--- tests/Feature/TrustedProxyRedirectTest.php
+++ tests/Feature/TrustedProxyRedirectTest.php
@@ -9,11 +9,13 @@ class TrustedProxyRedirectTest extends TestCase
     public function test_auth_redirect_uses_the_request_host_instead_of_a_malformed_forwarded_host(): void
     {
         $this
+            ->withHeaders([
+                'Host' => 'gct-systems.onrender.com',
+                'X-Forwarded-Host' => 'https',
+                'X-Forwarded-Proto' => 'https',
+                'X-Forwarded-Port' => '443',
+            ])
             ->withServerVariables([
-                'HTTP_HOST' => 'gct-systems.onrender.com',
-                'HTTP_X_FORWARDED_HOST' => 'https',
-                'HTTP_X_FORWARDED_PROTO' => 'https',
-                'HTTP_X_FORWARDED_PORT' => '443',
                 'REMOTE_ADDR' => '127.0.0.1',
             ])
             ->get('/topbar/summary')
