9cf7a2d0ef22b968f666f9360d59e3535eaf7615
oclo <kurtvergara06@gmail.com>
2026-08-09 12:28:38 +0800

    Hide PO status action after Purchase handoff

--- resources/views/components/ui/action-button.blade.php
+++ resources/views/components/ui/action-button.blade.php
@@ -8,9 +8,6 @@
 ])
 
 @php
-  /**
-   * Action Button Component - Reusable action button with Font Awesome icons
-   */
   $iconMap = [
     'view' => 'fa-eye',
     'edit' => 'fa-pen-to-square',
@@ -35,14 +32,18 @@
 
   $titleAttr = $title ? ' title="' . $title . '"' : '';
   $disabledAttr = $disabled ? ' disabled' : '';
+  $currentStatus = (string) $attributes->get('data-current-status', '');
+  $shouldRender = ! ($type === 'status' && $currentStatus !== '' && $currentStatus !== 'Ordered');
 @endphp
 
-@if($href)
-  <a href="{{ $href }}" class="{{ $buttonClass }}"{{ $titleAttr }} {{ $attributes }}>
-    <i class="fa-solid {{ $icon }}"></i>
-  </a>
-@else
-  <button type="{{ $buttonType }}" class="{{ $buttonClass }}"{{ $titleAttr }}{{ $disabledAttr }} {{ $attributes }}>
-    <i class="fa-solid {{ $icon }}"></i>
-  </button>
+@if($shouldRender)
+  @if($href)
+    <a href="{{ $href }}" class="{{ $buttonClass }}"{{ $titleAttr }} {{ $attributes }}>
+      <i class="fa-solid {{ $icon }}"></i>
+    </a>
+  @else
+    <button type="{{ $buttonType }}" class="{{ $buttonClass }}"{{ $titleAttr }}{{ $disabledAttr }} {{ $attributes }}>
+      <i class="fa-solid {{ $icon }}"></i>
+    </button>
+  @endif
 @endif
