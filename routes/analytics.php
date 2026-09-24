aa23ca9e7f324b82316648c46eb3bd433c8b0c58
oclo <kurtvergara06@gmail.com>
2026-09-24 10:52:27 +0800

    feat(analytics): add inventory prediction route

--- routes/analytics.php
+++ routes/analytics.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\Admin\AnalyticsStageController;
 use App\Http\Controllers\Admin\DelayPredictionController;
 use App\Http\Controllers\Admin\DescriptiveAnalyticsController;
 use App\Http\Controllers\Admin\FuelPredictionController;
+use App\Http\Controllers\Admin\InventoryPredictionController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth')->prefix('analytics')->group(function (): void {
@@ -30,6 +31,9 @@ Route::middleware('auth')->prefix('analytics')->group(function (): void {
 
     Route::get('/fuel-predictions', [FuelPredictionController::class, 'index'])
         ->name('analytics.fuel-predictions');
+
+    Route::post('/inventory-predictions', [InventoryPredictionController::class, 'index'])
+        ->name('analytics.inventory-predictions');
 
     Route::get('/{stage}', [AnalyticsStageController::class, 'show'])
         ->whereIn('stage', ['diagnostic', 'predictive', 'prescriptive'])
