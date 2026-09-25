7ecccd387fb61d5e60fb7ff3b7a213504dc79dd0
kurtvergara06-rgb <kurtvergara06@gmail.com>
2026-09-25 18:12:50 +0800

    style(maintenance): align Operation Referrals toolbar with Job Orders layout and styling

--- resources/views/Maintenance/referrals.blade.php
+++ resources/views/Maintenance/referrals.blade.php
@@ -44,20 +44,22 @@
                     </div>
                 </div>
 
-                <form method="GET" action="{{ route('maintenance-referrals') }}" class="toolbar referral-toolbar">
-                    <div class="search-box">
-                        <i class="fa-solid fa-magnifying-glass"></i>
-                        <input type="text" name="search" value="{{ request('search') }}" placeholder="Search incident, bus, location, or problem...">
-                    </div>
+                <x-ui.table-toolbar
+                    :action="route('maintenance-referrals')"
+                    class="toolbar referral-toolbar"
+                    search-placeholder="Search incident, bus, location, or problem..."
+                    :show-button="false"
+                >
                     <div class="filter-group">
-                        <select name="status" onchange="this.form.submit()">
+                        <label for="referralStatusFilter"></label>
+                        <select name="status" id="referralStatusFilter" onchange="this.form.submit()">
                             <option value="all">All Statuses</option>
                             @foreach(['Pending', 'Approved', 'Job Order Created', 'Rejected'] as $status)
                                 <option value="{{ $status }}" @selected(request('status') === $status)>{{ $status }}</option>
                             @endforeach
                         </select>
                     </div>
-                </form>
+                </x-ui.table-toolbar>
 
                 <div class="table-wrap">
                     <table class="referrals-table">
